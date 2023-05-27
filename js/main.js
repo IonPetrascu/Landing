e0877fee4e280d8656e43a03e40a3887ef79a6ec
IonPetrascu <118193126+IonPetrascu@users.noreply.github.com>
2023-05-27 19:25:27 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ AOS.init({
 	
 });
 
-/* Nav icon */
+/* nav icon */
 const navBtn = document.querySelector('.nav-icon-btn');
 const navIcon = document.querySelector('.nav-icon');
-const nav = document.querySelector('.header__top-row');
+const nav = document.querySelector('.header__top');
 
 navBtn.onclick = function () {
     navIcon.classList.toggle('nav-icon--active');
-    nav.classList.toggle('header__top-row--mobile');
+    nav.classList.toggle('header__top-mobile');
     document.body.classList.toggle('no-scroll');
 }
